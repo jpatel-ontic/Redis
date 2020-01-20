ebfc96c0fbcf94e6fc6f9509bcdaacf4a9a60280
Jeet Patel <eagleye@JeetAs-MBP.Dlink>
2020-01-20 10:15:25 +0530

    change name to Main from Rest4Application

--- src/main/java/com/example/rest4/Main.java
+++ src/main/java/com/example/rest4/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Rest4Application {
+public class Main {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(Rest4Application.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
